6e4c870a573ba77cf9e75bb6984f2e50ed2c7b72
ET0E <ingramrt@mail.gvsu.edu>
2024-02-08 09:52:35 -0500

    Added files to different directory

---
+++ CIS452_Lab5/lab04_c.cpp
@@ -0,0 +1,27 @@
+// Compile with -lpthread flag
+#include <iostream>
+#include <thread>
+#include <unistd.h>
+
+using namespace std;
+
+void do_greeting();
+
+int main() {
+    cout << "From PID " << getpid()
+        << " thread id " << std::this_thread::get_id() << endl;
+
+    // start thread at a function
+    std::thread one(do_greeting);
+    usleep(2000000);
+
+    one.join();  // join() or detach() is required
+    return 0;
+}
+
+void do_greeting() {
+    std::this_thread::sleep_for(chrono::seconds(1));
+
+    cout << "Thread version of Hello, world. PID=" << getpid() 
+        << " TID=" << std::this_thread::get_id() << endl;
+}
