264f28f7a0febce6a76038fb634feb8a272e5519
ET0E <ingramrt@mail.gvsu.edu>
2024-02-22 17:03:56 -0500

    turn in, ready

--- CIS452_Lab6/reader.c
+++ CIS452_Lab6/reader.c
@@ -4,15 +4,24 @@
 #include <sys/stat.h>
 #include <sys/ipc.h>
 #include <sys/shm.h>
+#include <string.h>
 
 
 #define SHM_SIZE 1024
 
-int main(){
+struct sharedData{
+    int writeDone;
+    int readDone;
+    int close_write;
+    char message[SHM_SIZE];
+};
+
+int main(int argc, char* argv[]){
     key_t mykey = ftok("/tmp", 0xa04f01);
     int shmId;
-    char* shmPtr;
-    char* user_input;
+    struct sharedData* sharedData;
+
+    int reader = atoi(argv[1]);
 
     if ((shmId =
          shmget (mykey, SHM_SIZE, S_IRUSR | S_IWUSR)) < 0) {
@@ -20,20 +29,32 @@ int main(){
         exit (1);
     }
 
-    if ((shmPtr = shmat (shmId, NULL, 0)) == (void *) -1) {
+    if ((sharedData = shmat (shmId, NULL, 0)) == (void *) -1) {
         perror ("can't attach\n");
         exit (1);
     }
 
-    while(1){
-        printf("User Input: ");
-        scanf("%s\n", user_input);
+    printf("Reader Process Ready \n");
+    sharedData->close_write = 0;
 
-        if(strcmp(user_input, "quit")){
-            exit(1);
+    while(1){
+        
+        while(sharedData->writeDone == 0){};
+
+        if(sharedData->close_write == 1){
+            break;
         }
 
-        strncpy(shmPtr, user_input, SHM_SIZE);
+        printf("Message from Writer to reader %d: %s", reader, sharedData->message);
+
+        sharedData->readDone = 1;
+        sharedData->writeDone = 0;
     }
 
+    if (shmdt (sharedData) < 0) {
+        perror ("just can't let go\n");
+        exit (1);
+    }
+
+    return 0;
 }
--- CIS452_Lab6/writer.c
+++ CIS452_Lab6/writer.c
@@ -4,6 +4,7 @@
 #include <sys/stat.h>
 #include <sys/ipc.h>
 #include <sys/shm.h>
+#include <string.h>
 
 
 #define SHM_SIZE 1024
@@ -11,35 +12,55 @@
 struct sharedData{
     int writeDone;
     int readDone;
+    int close_write;
     char message[SHM_SIZE];
 };
 
 int main(){
     key_t mykey = ftok("/tmp", 0xa04f01);
     int shmId;
-    char* shmPtr;
-    char* user_input;
+    char user_input[30];
+    struct sharedData* sharedData;
 
     if ((shmId =
-         shmget (mykey, SHM_SIZE, IPC_CREAT | S_IRUSR | S_IWUSR)) < 0) {
+         shmget (mykey, sizeof(struct sharedData), IPC_CREAT | S_IRUSR | S_IWUSR)) < 0) {
         perror ("i can't get no..\n");
         exit (1);
     }
 
-    if ((shmPtr = shmat (shmId, NULL, 0)) == (void *) -1) {
+    if ((sharedData = shmat (shmId, NULL, 0)) == (void *) -1) {
         perror ("can't attach\n");
         exit (1);
     }
+
+    sharedData->readDone = 1;
+    sharedData->close_write = 0;
     
     while(1){
-        printf("User Input: ");
-        scanf("%s\n", user_input);
 
-        if(strcmp(user_input, "quit")){
-            exit(1);
+        printf("User Input: ");
+        fgets(user_input, sizeof(user_input), stdin);
+
+        if(strcmp(user_input, "quit\n") == 0){
+            sharedData->close_write = 1;
+            break;
         }
 
-        strncpy(shmPtr, user_input, SHM_SIZE);
+        while(sharedData->readDone == 0){};
+
+        strncpy(sharedData->message, user_input, SHM_SIZE);
+
+        sharedData->writeDone = 1;
     }
 
+    sharedData->writeDone = 1;
+
+    if (shmdt (sharedData) < 0) {
+        perror ("just can't let go\n");
+        exit (1);
+    }
+
+    shmctl(shmId, IPC_RMID, 0);
+
+    return 0;
 }
