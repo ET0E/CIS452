865efeb77844ad312a8747659dac7ed2fa0710e0
ET0E <ingramrt@mail.gvsu.edu>
2024-02-22 15:03:54 -0500

    update reader and writer code

---
+++ CIS452_Lab6/writer.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/ipc.h>
+#include <sys/shm.h>
+
+
+#define SHM_SIZE 1024
+
+struct sharedData{
+    int writeDone;
+    int readDone;
+    char message[SHM_SIZE];
+};
+
+int main(){
+    key_t mykey = ftok("/tmp", 0xa04f01);
+    int shmId;
+    char* shmPtr;
+    char* user_input;
+
+    if ((shmId =
+         shmget (mykey, SHM_SIZE, IPC_CREAT | S_IRUSR | S_IWUSR)) < 0) {
+        perror ("i can't get no..\n");
+        exit (1);
+    }
+
+    if ((shmPtr = shmat (shmId, NULL, 0)) == (void *) -1) {
+        perror ("can't attach\n");
+        exit (1);
+    }
+    
+    while(1){
+        printf("User Input: ");
+        scanf("%s\n", user_input);
+
+        if(strcmp(user_input, "quit")){
+            exit(1);
+        }
+
+        strncpy(shmPtr, user_input, SHM_SIZE);
+    }
+
+}
