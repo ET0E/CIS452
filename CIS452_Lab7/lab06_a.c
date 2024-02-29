ed3d9f344730c55192c12079a75da7c82760c5e7
ET0E <ingramrt@mail.gvsu.edu>
2024-02-29 13:54:46 -0500

    Added lab7

---
+++ CIS452_Lab7/lab06_a.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+
+void* swapper(void*);
+
+int arr[2];
+
+int main(int argc, char* argv[]) {
+    pthread_t who;
+    long int loop;
+
+    loop = (int)argv[0];
+
+    arr[0] = 0;
+    arr[1] = 1;
+    pthread_create(&who, NULL, swapper, &loop);
+    for (int k = 0; k < loop; k++) {
+        int temp;
+        temp = arr[0];
+        arr[0] = arr[1];
+        arr[1] = temp;
+    }
+    int rc;
+    pthread_join(who, (void **) &rc);
+    printf ("Values: %5d %5d\n", arr[0], arr[1]);
+}
+
+void* swapper(void *arg) {
+    long int *num = (long int *) arg;
+    for (int k = 0; k < *num; k++) {
+        int temp;
+        temp = arr[0];
+        arr[0] = arr[1];
+        arr[1] = temp;
+    }
+    return 0;
+}
