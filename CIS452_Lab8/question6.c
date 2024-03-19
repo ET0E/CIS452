cb33ea87557777efe0a7227f8472976d6909c8fe
ET0E <ingramrt@mail.gvsu.edu>
2024-03-19 19:09:30 -0400

    jkljl

--- CIS452_Lab8/question6.c
+++ CIS452_Lab8/question6.c
@@ -5,7 +5,25 @@
 #include <unistd.h>
 
 int main(){
+    struct sched_param param;
+
     int foo = sched_getscheduler(getpid());
     printf("%d\n", foo);
+
+    switch (foo) {
+        case SCHED_OTHER:
+            printf("Scheduling Policy: SCHED_OTHER\n");
+            break;
+        case SCHED_FIFO:
+            printf("Scheduling Policy: SCHED_FIFO\n");
+            break;
+        case SCHED_RR:
+            printf("Scheduling Policy: SCHED_RR\n");
+            break;
+        default:
+            printf("Unknown Scheduling Policy\n");
+            break;
+    }
+
     return 0;
 }
